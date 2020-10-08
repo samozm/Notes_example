6889f561edfad2fc998fe43fc0f201301b9cb4fb
Zijun Feng <zfeng66@wisc.edu>
2020-10-08 13:18:54 -0500

    add shebang and permission for countWeek.sh

--- countWeek.sh
+++ countWeek.sh
@@ -1,2 +1,7 @@
+#!/bin/bash
+set -e # script terminates if any command exits with non-zero status
+set -u # terminates if any variable is unset
+set -o pipefail # terminates if command within a pipes exits unsuccessfully
+
 grep '## [Ww]eek' git-notes.md | wc -l
 grep git git-notes.md | wc -l
